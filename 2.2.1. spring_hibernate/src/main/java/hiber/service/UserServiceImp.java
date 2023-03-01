95cd970bc85f6d954856b9902bf57e38ff278840
nikonpavlenko <nikonpavlenko@gmail.com>
2023-03-01 13:49:30 +1100

    update

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -11,26 +11,30 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+    private final UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> getUsersByCarModel(String model, int series) {
-      return userDao.getUsersByCarModel(model, series);
-   }
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> getUsersByCarModel(String model, int series) {
+        return userDao.getUsersByCarModel(model, series);
+    }
 
 
 }
